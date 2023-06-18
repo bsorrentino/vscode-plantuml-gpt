6d3a19abb624f7716177225408264d8a93c83f58
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-06-18 23:04:27 +0200

    feat: add saved prompts
    add saved prompts in a separate tab

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { Configuration, OpenAIApi } from 'openai';
+import * as https from 'node:https';
 
 // [How to use the VSCode local storage API](https://www.chrishasz.com/blog/2020/07/28/vscode-how-to-use-local-storage-api/)
 class LocalStorageService {
@@ -47,6 +48,102 @@ class LocalStorageService {
 	private _setValue<T>(key : string, value : T ){
 		this.storage.update(key, value );
 	}
+}
+
+
+class PromptsSaved {
+
+	private _promptSaved:Array<string>;
+
+	constructor( private storage: LocalStorageService ) {
+		this._promptSaved = storage.getPrompts();
+	}
+
+	get content() {
+
+		return this._promptSaved.map( (p,i) => 
+			`<tr>
+			<td>${p}</td>
+			<td class="nowrap">
+			<vscode-button class="table-command" data-command="saved.replace" data-index="${i}" appearance="icon" aria-label="Replace">
+				<span class="codicon codicon-reply"></span>
+			</vscode-button>
+			<vscode-button class="table-command"  data-command="saved.delete" data-index="${i}" appearance="icon" aria-label="Delete">
+				<span class="codicon codicon-trash"></span>
+			</vscode-button>
+			</td>
+			</tr>`).join('\n');
+
+	}
+
+	get length() { return  this._promptSaved.length; } 
+
+	getPrompt( index: number ) {
+		return ( index >= 0 && index < this._promptSaved.length) ? this._promptSaved[index] : null;
+	} 
+
+	addPrompt( prompt: string ) {
+		this._promptSaved.push( prompt );
+		this.storage.addPrompt( prompt );
+	}
+
+	removePrompt( index: number ) {
+		if( index >= 0 && index < this._promptSaved.length) {
+			this._promptSaved.splice(index, 1);	
+			this.storage.removePromptAtIndex( index );
+			return true;
+		}
+		return false;
+	}
+}
+
+class PromptsHistory {
+
+	private _promptHistory:Array<string> ;
+
+	constructor(  ) { 
+		this._promptHistory = [];
+		// _promptHistory.concat(  Array<string>(10).fill("Prompt ahgsdgasdfasdfahgfdagsdfahgsdfahgfsdagfghfadsfahdgf") );
+	}
+
+	get content() {
+
+		return this._promptHistory.map( (p,i) => 
+			`<tr>
+			<td>${p}</td>
+			<td class="nowrap">
+			<vscode-button class="table-command" data-command="history.replace" data-index="${i}" appearance="icon" aria-label="Replace">
+				<span class="codicon codicon-reply"></span>
+			</vscode-button>
+			<vscode-button class="table-command"  data-command="history.delete" data-index="${i}" appearance="icon" aria-label="Delete">
+				<span class="codicon codicon-trash"></span>
+			</vscode-button>
+			<vscode-button class="table-command"  data-command="history.save" data-index="${i}" appearance="icon" aria-label="Save">
+				<span class="codicon codicon-save"></span>
+			</vscode-button>
+			</td>
+			</tr>`).join('\n');
+
+	}
+
+	get length() { return  this._promptHistory.length; } 
+
+	getPrompt( index: number ) {
+		return ( index >= 0 && index < this._promptHistory.length) ? this._promptHistory[index] : null;
+	} 
+
+	addPrompt( prompt: string ) {
+		this._promptHistory.push( prompt );
+	}
+
+	removePrompt( index: number ) {
+		if( index >= 0 && index < this._promptHistory.length) {
+			this._promptHistory.splice(index, 1);	
+			return true;
+		}
+		return false;
+	}
+
 }
 
 // This method is called when your extension is activated
@@ -222,13 +319,13 @@ class PlantUMLGPTProvider implements vscode.WebviewViewProvider {
 	private _view: vscode.WebviewView|null = null;
 	private _disposables: vscode.Disposable[] = [];
 	private _undoText:string|null = null;
-	private _promptHistory:Array<string>;
+	private _promptHistory = new PromptsHistory();
+	private _promptSaved:PromptsSaved;
 
-	constructor( private readonly _extensionUri: vscode.Uri, private storage: LocalStorageService ) { 
+	constructor( private readonly _extensionUri: vscode.Uri, storage: LocalStorageService ) { 
 		// console.log( 'PlantUMLGPTProvider', _extensionUri);	
 
-		this._promptHistory = storage.getPrompts();
-		// _promptHistory.concat(  Array<string>(10).fill("Prompt ahgsdgasdfasdfahgfdagsdfahgsdfahgfsdagfghfadsfahdgf") );
+		this._promptSaved = new PromptsSaved(storage);
 }
 
 	public resolveWebviewView(	webviewView: vscode.WebviewView, 
@@ -261,11 +358,6 @@ class PlantUMLGPTProvider implements vscode.WebviewViewProvider {
 		webview.html = this._getWebviewContent(webviewView.webview);
 
 		this._disposables.push( this._addMessageHandler(webview) );
-
-		this._sendMessageToWebView( 'history.update', {
-			tbody: this._getHistoryBodyContent(),
-			length: this._promptHistory.length
-		});
 
 	}
 
@@ -301,9 +393,9 @@ class PlantUMLGPTProvider implements vscode.WebviewViewProvider {
 					this._submitAndReplace( text )
 					.then( result => {			
 						this._undoText = result.input;
-						this._promptHistory.push( text );
+						this._promptHistory.addPrompt( text );
 						this._sendMessageToWebView( 'history.update', {
-							tbody: this._getHistoryBodyContent(),
+							tbody: this._promptHistory.content,
 							length: this._promptHistory.length
 						});
 					})
@@ -318,23 +410,42 @@ class PlantUMLGPTProvider implements vscode.WebviewViewProvider {
 					this._undo();
 					
 					return;
+				case 'saved.replace':
+				{	
+					const index = parseInt(text);
+					const prompt = this._promptSaved.getPrompt(index);
+					if( prompt ) {
+						this._sendMessageToWebView( 'prompt.replace', prompt);
+					}
+					return;
+				}		
 				case 'history.replace':
 				{	
 					const index = parseInt(text);
-					if( index >= 0 && index < this._promptHistory.length) {
-						this._sendMessageToWebView( 'prompt.replace', this._promptHistory[index]	);
+					const prompt = this._promptHistory.getPrompt(index);
+					if( prompt ) {
+						this._sendMessageToWebView( 'prompt.replace', prompt);
 					}
 					return;
 				}
 				case 'history.delete':
 				{
 					const index = parseInt(text);
-					if( index >= 0 && index < this._promptHistory.length) {
-						this._promptHistory.splice(index, 1);	
-						this.storage.removePromptAtIndex( index );
+					if( this._promptHistory.removePrompt( index ) ) {
 						this._sendMessageToWebView( 'history.update', {
-							tbody: this._getHistoryBodyContent(),
+							tbody: this._promptHistory.content,
 							length: this._promptHistory.length
+						});
+					}
+					return;
+				}
+				case 'saved.delete':
+				{
+					const index = parseInt(text);
+					if( this._promptSaved.removePrompt( index ) ) {
+						this._sendMessageToWebView( 'saved.update', {
+							tbody: this._promptSaved.content,
+							length: this._promptSaved.length
 						});
 					}
 					return;
@@ -342,8 +453,13 @@ class PlantUMLGPTProvider implements vscode.WebviewViewProvider {
 				case 'history.save':
 				{
 					const index = parseInt(text);
-					if( index >= 0 && index < this._promptHistory.length) {
-						this.storage.addPrompt( this._promptHistory[index] );
+					const prompt = this._promptHistory.getPrompt(index);
+					if( prompt ) {
+						this._promptSaved.addPrompt( prompt );
+						this._sendMessageToWebView( 'saved.update', {
+							tbody: this._promptSaved.content,
+							length: this._promptSaved.length
+						});
 					}
 					return;
 				}
@@ -351,26 +467,6 @@ class PlantUMLGPTProvider implements vscode.WebviewViewProvider {
 
 
 		}, undefined, this._disposables);
-
-	}
-
-	private _getHistoryBodyContent() {
-
-		return this._promptHistory.map( (p,i) => 
-			`<tr>
-			<td>${p}</td>
-			<td class="nowrap">
-			<vscode-button class="history-command" data-command="history.replace" data-index="${i}" appearance="icon" aria-label="Replace">
-				<span class="codicon codicon-reply"></span>
-			</vscode-button>
-			<vscode-button class="history-command"  data-command="history.delete" data-index="${i}" appearance="icon" aria-label="Delete">
-				<span class="codicon codicon-trash"></span>
-			</vscode-button>
-			<vscode-button class="history-command"  data-command="history.save" data-index="${i}" appearance="icon" aria-label="Save">
-				<span class="codicon codicon-save"></span>
-			</vscode-button>
-			</td>
-			</tr>`).join('\n');
 
 	}
 
@@ -408,6 +504,7 @@ class PlantUMLGPTProvider implements vscode.WebviewViewProvider {
  <vscode-panels id="panels" aria-label="Default">
   <vscode-panel-tab id="tab-prompt">Prompt</vscode-panel-tab>
   <vscode-panel-tab id="tab-history">History<vscode-badge id="history_badge">${this._promptHistory.length}</vscode-badge></vscode-panel-tab>
+  <vscode-panel-tab id="tab-saved">Saved<vscode-badge id="saved_badge">${this._promptSaved.length}</vscode-badge></vscode-panel-tab>
   <vscode-panel-view id="wiew-1">
    <div id="prompt_container">
     <vscode-text-area id="prompt" rows="10" cols="80" resize="horizontal" ${submitDisabledTag('readonly')} placeholder="${ submitDisabled ? 'Please provides API KEY in extension settings' : 'Let chat with PlantUML diagram'}"></vscode-text-area>
@@ -428,10 +525,27 @@ class PlantUMLGPTProvider implements vscode.WebviewViewProvider {
 				</tr>
 			</thead>
 			<tbody>
+				${this._promptHistory.content}
 			</tbody>
 		</table>
 	</div>
   </vscode-panel-view>
+  <vscode-panel-view id="view-2">
+  <div class="table-scroll">
+	  <table id="saved_prompt">
+		  <thead>
+			  <tr>
+				  <th>Prompt</th>
+				  <th>Actions</th>
+			  </tr>
+		  </thead>
+		  <tbody>
+			  ${this._promptSaved.content}
+		  </tbody>
+	  </table>
+  </div>
+</vscode-panel-view>
+
  </vscode-panels>
  <script type="module" nonce="${nonce}" src="${webViewJSUri}"></script>
 
@@ -455,9 +569,9 @@ class PlantUMLGPTProvider implements vscode.WebviewViewProvider {
 			throw 'no active editor!';
 		}
 
-		const input = _getAllTextFromEditor( activeTextEditor);
+		const input = _getAllTextFromEditor( activeTextEditor) ?? '';
 
-		if( !( instruction && instruction.length>0) ) {
+		if( !( instruction && instruction.length > 0) ) {
 			// eslint-disable-next-line no-throw-literal
 			throw 'instruction is empty!';
 		}
@@ -468,16 +582,30 @@ class PlantUMLGPTProvider implements vscode.WebviewViewProvider {
 		const openai = new OpenAIApi(configuration);
 		
 		try {
+			
+
+			// const response = await openaiRawRequest( apikey,{
+			// 	model: "text-davinci-edit-001",
+			// 	input: input.replace(/(?:\r\n|\r|\n)+/g, '\n'),
+			// 	instruction: instruction,
+			// 	temperature: 0,
+			// 	// eslint-disable-next-line @typescript-eslint/naming-convention
+			// 	top_p: 1,
+			// });
+			// const result = response.choices[0].text;
+
 			const response = await openai.createEdit({
 				model: "text-davinci-edit-001",
-				input: input,
+				input: input.replace(/(?:\r\n|\r|\n)+/g, '\n'),
 				instruction: instruction,
-				temperature: 0,
-				['top_p']: 1,
+				temperature: 0.5,
+				// eslint-disable-next-line @typescript-eslint/naming-convention
+				top_p: 1,
 			});
 
 			const result = response.data.choices[0].text;
 
+			console.log( result );
 			if( result ) {
 				_replaceTextInEditor( activeTextEditor, result );
 			}
@@ -494,4 +622,80 @@ class PlantUMLGPTProvider implements vscode.WebviewViewProvider {
 		}
 
 	}
+
+}
+
+
+interface OpenAIResult {
+	object: string,
+	created: number,
+	choices: Array<{ text: string, index: number }>,
+	usage: {
+		  // eslint-disable-next-line @typescript-eslint/naming-convention
+		  prompt_tokens: number,
+		  // eslint-disable-next-line @typescript-eslint/naming-convention
+		  completion_tokens: number,
+		  // eslint-disable-next-line @typescript-eslint/naming-convention
+		  total_tokens: number
+		}
+}
+
+function openaiRawRequest( 
+	apiKey: string,
+	postData: {
+		model: string,
+		input: string|null,
+		instruction: string,
+		temperature: number,
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		top_p: number,
+}): Promise<OpenAIResult>
+{
+
+	const body = JSON.stringify(postData);
+	const options = {
+		hostname: "api.openai.com",
+		port: 443,
+		path: "/v1/edits",
+		method: 'POST',
+		headers: {
+		  // eslint-disable-next-line @typescript-eslint/naming-convention
+		  'Content-Type': 'application/json',
+		  // eslint-disable-next-line @typescript-eslint/naming-convention
+		  // 'Content-Length': Buffer.byteLength(body),
+		  // eslint-disable-next-line @typescript-eslint/naming-convention
+		  'Authorization': `Bearer ${apiKey}`
+		}
+	  };
+	
+	  return new Promise<OpenAIResult>( (resolve, reject) => {
+		//change to http for local testing
+		const req = https.request(options, res => {
+
+			res.setEncoding('utf8');
+		
+			let result = '';
+		
+			res.on('data', chunk => result = result + chunk );
+		
+			res.on('end',() => {
+			
+				if (res.statusCode !== 200) {
+					reject( res );
+				} else {
+					resolve( JSON.parse(result) );
+				}
+			});
+		
+		});
+		
+		req.on('error', e =>  reject(e) );
+		
+		// write data to request body
+		req.write(body);
+		req.end();
+
+	  });
+	
+	
 }
